c844342028c882de177849b4948cf56c5528fc84
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-11-29 13:04:19 +0200

    Re-set Vue configuration and add Vuetify for material

---
+++ backoffice/client/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="3">
+      <h1 class="text-center">
+        asdasdasd
+      </h1>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
