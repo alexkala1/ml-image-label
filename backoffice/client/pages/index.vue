f4e2817c9aca04dbb1361bf502ea8ead983e937f
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-12-30 22:04:08 +0200

    Add mock cards and pagination components

--- backoffice/client/pages/index.vue
+++ backoffice/client/pages/index.vue
@@ -1,83 +1,47 @@
 <template>
-	<v-row justify="center" align="center">
-		<v-col cols="12" sm="8" md="6">
-			<div class="text-center">
-				<logo />
-				<vuetify-logo />
-			</div>
-			<v-card>
-				<v-card-title class="headline">
-					Welcome to the Vuetify + Nuxt.js template
-				</v-card-title>
-				<v-card-text>
-					<p>
-						Vuetify is a progressive Material Design component
-						framework for Vue.js. It was designed to empower
-						developers to create amazing applications.
-					</p>
-					<p>
-						For more information on Vuetify, check out the
-						<a
-							href="https://vuetifyjs.com"
-							target="_blank"
-							rel="noopener noreferrer"
+	<v-container>
+		<v-row justify="center" align="center">
+			<v-col cols="4" sm="6" md="3" v-for="i in 10" :key="i">
+				<v-skeleton-loader
+					:loading="loading"
+					:transition="transition"
+					type="card, actions"
+				>
+					<v-card class="mx-auto" max-width="400">
+						<v-img
+							class="white--text align-end"
+							height="200px"
+							src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
 						>
-							documentation </a
-						>.
-					</p>
-					<p>
-						If you have questions, please join the official
-						<a
-							href="https://chat.vuetifyjs.com/"
-							target="_blank"
-							rel="noopener noreferrer"
-							title="chat"
+							<v-card-title
+								>Top 10 Australian beaches</v-card-title
+							>
+						</v-img>
+						<v-card-subtitle class="pb-0"
+							>Number {{ i }}</v-card-subtitle
 						>
-							discord </a
-						>.
-					</p>
-					<p>
-						Find a bug? Report it on the github
-						<a
-							href="https://github.com/vuetifyjs/vuetify/issues"
-							target="_blank"
-							rel="noopener noreferrer"
-							title="contribute"
-						>
-							issue board </a
-						>.
-					</p>
-					<p>
-						Thank you for developing with Vuetify and I look forward
-						to bringing more exciting features in the future.
-					</p>
-					<div class="text-xs-right">
-						<em><small>&mdash; John Leider</small></em>
-					</div>
-					<hr class="my-3" />
-					<a
-						href="https://nuxtjs.org/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Nuxt Documentation
-					</a>
-					<br />
-					<a
-						href="https://github.com/nuxt/nuxt.js"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Nuxt GitHub
-					</a>
-				</v-card-text>
-				<v-card-actions>
-					<v-spacer />
-					<v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-				</v-card-actions>
-			</v-card>
-		</v-col>
-	</v-row>
+						<v-card-text class="text--primary">
+							<div>Whitehaven Beach</div>
+							<div>Whitsunday Island, Whitsunday Islands</div>
+						</v-card-text>
+						<v-card-actions>
+							<v-btn color="orange" text> Share </v-btn>
+							<v-btn color="orange" text> Explore </v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-skeleton-loader>
+			</v-col>
+		</v-row>
+		<v-row justify="center" align="center">
+			<v-pagination
+				v-model="page"
+				class="my-4"
+				:total-visible="7"
+				:length="15"
+				circle
+			></v-pagination>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
@@ -85,14 +49,26 @@ import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
+	data() {
+		return {
+			loading: true,
+			page: 1,
+			transition: 'scale-transition',
+		}
+	},
 	components: {
 		Logo,
 		VuetifyLogo,
 	},
-	methods: {
-		async logout() {
-			await this.$auth.logout()
-    },
-  }
+
+	methods: {},
+
+	mounted() {
+		this.$nextTick(function () {
+			window.setInterval(() => {
+				this.loading = false
+			}, 1000)
+		})
+	},
 }
 </script>
