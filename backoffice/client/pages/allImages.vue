0af5d9e5f37f596495226c2b9aa303a2870da63b
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-12-30 23:54:14 +0200

    Refactor style to index and add loading on paging

--- backoffice/client/pages/allImages.vue
+++ backoffice/client/pages/allImages.vue
@@ -1,139 +1,76 @@
 <template>
-	<v-row align="center" justify="center">
-		<v-col cols="12" sm="12" md="10">
-			<v-card>
-				<v-card-title>
-					Nutrition
-					<v-spacer></v-spacer>
-					<v-text-field
-						v-model="search"
-						append-icon="mdi-magnify"
-						label="Search"
-						single-line
-						hide-details
-					></v-text-field>
-				</v-card-title>
-				<v-data-table
-					:headers="headers"
-					:items="desserts"
-					:search="search"
+	<v-container>
+		<v-row justify="center" align="center">
+			<v-col cols="4" sm="6" md="3" v-for="i in 10" :key="i">
+				<v-skeleton-loader
 					:loading="loading"
-					loading-text="Loading... Please wait"
-				></v-data-table>
-			</v-card>
-		</v-col>
-	</v-row>
+					:transition="transition"
+					type="card, actions"
+				>
+					<v-card class="mx-auto" max-width="400">
+						<v-img
+							class="white--text align-end"
+							height="200px"
+							src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+						>
+							<v-card-title
+								>Top 10 Australian beaches</v-card-title
+							>
+						</v-img>
+						<v-card-subtitle class="pb-0"
+							>Number {{ i }}</v-card-subtitle
+						>
+						<v-card-text class="text--primary">
+							<div>Whitehaven Beach</div>
+							<div>Whitsunday Island, Whitsunday Islands</div>
+						</v-card-text>
+						<v-card-actions>
+							<v-btn color="orange" text> Share </v-btn>
+							<v-btn color="orange" text> Explore </v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-skeleton-loader>
+			</v-col>
+		</v-row>
+		<v-row justify="center" align="center" v-if="loading === false">
+			<v-pagination
+				v-model="page"
+				class="my-4"
+				:total-visible="7"
+				:length="15"
+				circle
+			></v-pagination>
+		</v-row>
+		<v-row justify="center" align="center" v-else>
+			<v-progress-circular class="my-4" indeterminate>
+      </v-progress-circular>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
 export default {
 	data() {
 		return {
-			search: '',
 			loading: true,
-			headers: [],
-			desserts: [],
+			page: 1,
+			transition: 'scale-transition',
 		}
 	},
+	components: {
+		Logo,
+		VuetifyLogo,
+	},
+
+	methods: {},
+
 	mounted() {
 		this.$nextTick(function () {
 			window.setInterval(() => {
 				this.loading = false
-				this.headers = [
-					{
-						text: 'Dessert (100g serving)',
-						align: 'start',
-						sortable: false,
-						value: 'name',
-					},
-					{ text: 'Calories', value: 'calories' },
-					{ text: 'Fat (g)', value: 'fat' },
-					{ text: 'Carbs (g)', value: 'carbs' },
-					{ text: 'Protein (g)', value: 'protein' },
-					{ text: 'Iron (%)', value: 'iron' },
-				]
-				this.desserts = [
-					{
-						name: 'Frozen Yogurt',
-						calories: 159,
-						fat: 6.0,
-						carbs: 24,
-						protein: 4.0,
-						iron: '1%',
-					},
-					{
-						name: 'Ice cream sandwich',
-						calories: 237,
-						fat: 9.0,
-						carbs: 37,
-						protein: 4.3,
-						iron: '1%',
-					},
-					{
-						name: 'Eclair',
-						calories: 262,
-						fat: 16.0,
-						carbs: 23,
-						protein: 6.0,
-						iron: '7%',
-					},
-					{
-						name: 'Cupcake',
-						calories: 305,
-						fat: 3.7,
-						carbs: 67,
-						protein: 4.3,
-						iron: '8%',
-					},
-					{
-						name: 'Gingerbread',
-						calories: 356,
-						fat: 16.0,
-						carbs: 49,
-						protein: 3.9,
-						iron: '16%',
-					},
-					{
-						name: 'Jelly bean',
-						calories: 375,
-						fat: 0.0,
-						carbs: 94,
-						protein: 0.0,
-						iron: '0%',
-					},
-					{
-						name: 'Lollipop',
-						calories: 392,
-						fat: 0.2,
-						carbs: 98,
-						protein: 0,
-						iron: '2%',
-					},
-					{
-						name: 'Honeycomb',
-						calories: 408,
-						fat: 3.2,
-						carbs: 87,
-						protein: 6.5,
-						iron: '45%',
-					},
-					{
-						name: 'Donut',
-						calories: 452,
-						fat: 25.0,
-						carbs: 51,
-						protein: 4.9,
-						iron: '22%',
-					},
-					{
-						name: 'KitKat',
-						calories: 518,
-						fat: 26.0,
-						carbs: 65,
-						protein: 7,
-						iron: '6%',
-					},
-				]
 			}, 2000)
 		})
 	},
