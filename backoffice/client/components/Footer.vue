973bb9c9f8c2dfab92ce7d8ea934aa25e0ab1851
Alex Kalaitzidis <alexkalaitzidis2@gmail.com>
2021-01-17 18:01:34 +0200

    Rename footer copyrights

--- backoffice/client/components/Footer.vue
+++ backoffice/client/components/Footer.vue
@@ -29,7 +29,7 @@
 			<v-divider></v-divider>
 
 			<v-card-text class="white--text">
-				{{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+				{{ new Date().getFullYear() }}, <strong>Kalaitzidis Alexandros — Kalotsi Bruno</strong>
 			</v-card-text>
 		</v-card>
 	</v-footer>
