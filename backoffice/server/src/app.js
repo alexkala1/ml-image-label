a3743febc62d8035ffc7c9b819eb11c7dbbb5d80
Alex Kalaitzidis <alexkalaitzidis2@gmail.com>
2021-01-17 13:27:02 +0200

    Add ping endpoint

--- backoffice/server/src/app.js
+++ backoffice/server/src/app.js
@@ -19,9 +19,7 @@ app.use(bodyParser.json({ limit: "50mb" }))
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 
 app.get('/', (req, res) => {
-	res.json({
-		message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄Hello World!'
-	});
+	res.send(true)
 });
 
 app.use('/api/v1', api);
