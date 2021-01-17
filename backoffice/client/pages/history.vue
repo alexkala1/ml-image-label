6d343c397e87e80122d959cc6232a2a1a8697e15
Alex Kalaitzidis <alexkalaitzidis2@gmail.com>
2021-01-17 19:08:23 +0200

    Add information in history datatable

--- backoffice/client/pages/history.vue
+++ backoffice/client/pages/history.vue
@@ -15,7 +15,7 @@
 				</v-card-title>
 				<v-data-table
 					:headers="headers"
-					:items="desserts"
+					:items="history"
 					:search="search"
 					:loading="loading"
 					loading-text="Loading... Please wait"
@@ -31,111 +31,41 @@ export default {
 		return {
 			search: '',
 			loading: true,
-			headers: [],
-			desserts: [],
+			headers: [
+				{ text: 'dataset', value: 'dataset' },
+				{ text: 'dataset_id', value: 'dataset_id' },
+				{ text: 'date', value: 'date' },
+				{ text: 'email', value: 'email' },
+				{ text: 'imageName', value: 'imageName' },
+				{ text: 'isHumanChecked', value: 'isHumanChecked' },
+				{ text: 'isVerified', value: 'isVerified' },
+				{ text: 'object', value: 'object' },
+				{ text: 'reviewedAt', value: 'reviewedAt' },
+				{ text: 'user_id', value: 'user_id' },
+				{ text: '__v', value: '__v' },
+				{ text: '_id', value: '_id' },
+			],
+			history: [],
 		}
 	},
-	mounted() {
-		this.$nextTick(function () {
-			window.setInterval(() => {
+
+	methods: {
+		async fetchHistory() {
+			try {
+				const { data } = await this.$axios.get(
+					'http://localhost:3001/api/v1/images/history'
+				)
+				this.history = data
 				this.loading = false
-				this.headers = [
-					{
-						text: 'Dessert (100g serving)',
-						align: 'start',
-						sortable: false,
-						value: 'name',
-					},
-					{ text: 'Calories', value: 'calories' },
-					{ text: 'Fat (g)', value: 'fat' },
-					{ text: 'Carbs (g)', value: 'carbs' },
-					{ text: 'Protein (g)', value: 'protein' },
-					{ text: 'Iron (%)', value: 'iron' },
-				]
-				this.desserts = [
-					{
-						name: 'Frozen Yogurt',
-						calories: 159,
-						fat: 6.0,
-						carbs: 24,
-						protein: 4.0,
-						iron: '1%',
-					},
-					{
-						name: 'Ice cream sandwich',
-						calories: 237,
-						fat: 9.0,
-						carbs: 37,
-						protein: 4.3,
-						iron: '1%',
-					},
-					{
-						name: 'Eclair',
-						calories: 262,
-						fat: 16.0,
-						carbs: 23,
-						protein: 6.0,
-						iron: '7%',
-					},
-					{
-						name: 'Cupcake',
-						calories: 305,
-						fat: 3.7,
-						carbs: 67,
-						protein: 4.3,
-						iron: '8%',
-					},
-					{
-						name: 'Gingerbread',
-						calories: 356,
-						fat: 16.0,
-						carbs: 49,
-						protein: 3.9,
-						iron: '16%',
-					},
-					{
-						name: 'Jelly bean',
-						calories: 375,
-						fat: 0.0,
-						carbs: 94,
-						protein: 0.0,
-						iron: '0%',
-					},
-					{
-						name: 'Lollipop',
-						calories: 392,
-						fat: 0.2,
-						carbs: 98,
-						protein: 0,
-						iron: '2%',
-					},
-					{
-						name: 'Honeycomb',
-						calories: 408,
-						fat: 3.2,
-						carbs: 87,
-						protein: 6.5,
-						iron: '45%',
-					},
-					{
-						name: 'Donut',
-						calories: 452,
-						fat: 25.0,
-						carbs: 51,
-						protein: 4.9,
-						iron: '22%',
-					},
-					{
-						name: 'KitKat',
-						calories: 518,
-						fat: 26.0,
-						carbs: 65,
-						protein: 7,
-						iron: '6%',
-					},
-				]
-			}, 2000)
-		})
+			} catch (error) {
+				alert(error)
+				this.loading = false
+			}
+		},
+	},
+
+	mounted() {
+		this.fetchHistory()
 	},
 }
 </script>
