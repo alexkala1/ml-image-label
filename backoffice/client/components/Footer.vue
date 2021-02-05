68917e6badd8eecd9897c6a463db647b2cb4770a
Alex Kalaitzidis <alexkalaitzidis2@gmail.com>
2021-02-05 21:19:15 +0200

    Fix footer

--- backoffice/client/components/Footer.vue
+++ backoffice/client/components/Footer.vue
@@ -1,35 +1,22 @@
 <template>
-	<v-footer dark padless>
-		<v-card flat tile class="lighten-1 white--text text-center">
+	<v-footer dark padless app absolute>
+		<v-card
+			flat
+			tile
+			class="white--text text-center"
+			width="100%"
+		>
 			<v-card-text>
-				<v-btn
-					v-for="icon in icons"
-					:key="icon"
-					class="mx-4 white--text"
-					icon
-				>
-					<v-icon size="24px">
-						{{ icon }}
-					</v-icon>
-				</v-btn>
-			</v-card-text>
-
-			<v-card-text class="white--text pt-0">
-				Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit
-				amet. Mauris cursus commodo interdum. Praesent ut risus eget
-				metus luctus accumsan id ultrices nunc. Sed at orci sed massa
-				consectetur dignissim a sit amet dui. Duis commodo vitae velit
-				et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat
-				augue vel ipsum ultrices, cursus iaculis dui sollicitudin.
-				Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci
-				varius natoque penatibus et magnis dis parturient montes,
-				nascetur ridiculus mus.
+				This is a proof of concept project for the master in Advanced
+				Technologies in Informatics and Computers created by Kalaitzidis
+				Alexandros and Kalotsi Bruno.
 			</v-card-text>
 
 			<v-divider></v-divider>
 
 			<v-card-text class="white--text">
-				{{ new Date().getFullYear() }}, <strong>Kalaitzidis Alexandros — Kalotsi Bruno</strong>
+				{{ new Date().getFullYear() }},
+				<strong>Kalaitzidis Alexandros — Kalotsi Bruno</strong>
 			</v-card-text>
 		</v-card>
 	</v-footer>
@@ -37,8 +24,6 @@
 
 <script>
 export default {
-	data: () => ({
-		icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
-	}),
+	data: () => ({}),
 }
 </script>
